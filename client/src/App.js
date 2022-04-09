9a364815cfd1fa0a292d5b6387e837828fdb72b1
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-09 21:42:26 +0100

    chore: install react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Home></Home>
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={Home} exact />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
