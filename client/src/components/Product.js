f77b0f22a1d5abd18b00557e42c12b08c800a045
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-09 21:37:27 +0100

    feat: add products card

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -5,11 +5,22 @@ export default function Product({ props }) {
   return (
     <>
       <Card className="my-3 p-3 rounded">
-        {/* <a href={`/product/${props.product._id}`}></a> */}
+        {/* <a href={`/product/${props.product._id}`}> */}
         <Card.Img variant="top" src={props.image} />
-        {/* <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-        </Card.Body> */}
+        {/* </a> */}
+        <Card.Body>
+          {/* <a href={`/product/${props._id}`}> */}
+          <Card.Title as="div">
+            <strong>{props.name}</strong>
+          </Card.Title>
+          {/* </a> */}
+          <Card.Text as="div">
+            <div className="my-3">
+              {props.rating} from {props.numReviews}
+            </div>
+          </Card.Text>
+          <Card.Text as="h3">£{props.price}</Card.Text>
+        </Card.Body>
       </Card>
     </>
   );
