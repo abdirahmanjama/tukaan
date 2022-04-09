be565330c9499ac9a7ccf797c5ed25776331fd04
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-09 17:02:20 +0100

    feat: add icons to nav bar, inject fontawesome from cdnjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Hello</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
