291577c46a8d53a9507e7fa8b288e24fecae22ea
Abdirahman Jama <45077645+abdirahmanjama@users.noreply.github.com>
2022-04-21 13:21:17 +0100

    fix ide warnings

--- backend/routes/products.ts
+++ backend/routes/products.ts
@@ -1,17 +1,13 @@
-import express from "express";
-import products from "../data/products";
-import { Request, Response } from "express";
-import { Product } from "../../shared/interfaces";
+import express, { Request, Response } from "express";
 import ProductModel from "../models/products";
 import asyncHandler from "express-async-handler";
-import Product from "../models/products";
 
 const router = express.Router();
 
 router.get(
   "/",
   asyncHandler(async (_req: Request, res: Response) => {
-    const products = await ProductModel.find({});
+    const products = ProductModel.find({});
     res.json(products);
   })
 );
@@ -19,7 +15,7 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req: Request, res: Response) => {
-    const product = await ProductModel.findById(req.params.id);
+    const product = ProductModel.findById(req.params.id);
 
     if (product) {
       res.json(product);
